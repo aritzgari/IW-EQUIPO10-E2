f44ca0e9771c99c07a7c85ada80f60c45ea43a8c
Unknown <gorka.gomez@opendeusto.es>
2020-04-19 12:47:02 +0200

    clases y modelos

--- Gestion_de_la_produccion/appGestion_de_la_produccion/admin.py
+++ Gestion_de_la_produccion/appGestion_de_la_produccion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . models import Proceso,Equipo,Empleado
 # Register your models here.
+
+
+admin.site.register(Proceso)
+admin.site.register(Equipo)
+admin.site.register(Empleado)
+
--- Gestion_de_la_produccion/appGestion_de_la_produccion/models.py
+++ Gestion_de_la_produccion/appGestion_de_la_produccion/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+
+class Equipo(models.Model):
+    modelo = models.CharField(max_length=25)
+    marca = models.CharField(max_length=25)
+    categoria = models.CharField(max_length=50)
+    fecha_adquision = models.DateField()
+    fecha_instalacion = models.DateField()
+
+    def __str__(self):
+        return f"modelo={self.modelo},marca={self.marca}"
+
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=50)
+    DNI = models.CharField(max_length=9)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    telefono = models.IntegerField()
+
+    def __str__(self):
+        return f"nombre={self.nombre},apellido={self.apellido},DNI={self.DNI}"
+
+
+class Proceso(models.Model):
+    equipo = models.ForeignKey(Equipo, on_delete=callable(Equipo.marca))
+    empleados_asignados = models.ManyToManyField(Empleado)
+    codigo_orden = models.CharField(max_length=10)
+    codigo_proceso = models.IntegerField()
+    nombre_proceso = models.CharField(max_length=50)
+    referencia = models.CharField(max_length=10)
+    inicio = models.DateField()
+    fin = models.DateField()
+
+
+    def __str__(self):
+        return f"codigoorden={self.codigoorden},nombreproceso={self.nombreproceso}"
