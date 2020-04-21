7ceb3ab081571ad34853cb5f18d39812988c9be4
Unknown <aritzgaritano@opendeusto.es>
2020-04-21 18:39:11 +0200

    Vistas Básicas

--- Gestion_de_la_produccion/appGestion_de_la_produccion/urls.py
+++ Gestion_de_la_produccion/appGestion_de_la_produccion/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path ('', views.index, name ="index")
+    path ('', views.index, name ="index"),
+    path ('equipo/<int:equipo_id>', views.detailEquipo, name="detailEquipo"),
+    path ('empleado/<int:empleado_id>', views.detailEmpleado, name="detailEmpleado"),
+    path ('proceso/<int:proceso_id>', views.detailProceso, name="detailProceso")
 ]
--- Gestion_de_la_produccion/appGestion_de_la_produccion/views.py
+++ Gestion_de_la_produccion/appGestion_de_la_produccion/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Equipo, Empleado, Proceso
 
 def index(request):
-    return HttpResponse ("Esta es la primera pagina de aqui directos a la entrega")
+    equipo = Equipo.objects.order_by("modelo")
+    output = ' , '.join([d.modelo for d in equipo])
+    empleado=Empleado.objects.order_by("nombre")
+    output2 = ' , '.join([d.nombre for d in empleado])
+    proceso=Proceso.objects.order_by("nombre_proceso")
+    output3 = ' , '.join([d.nombre_proceso for d in proceso])
+    return HttpResponse ("Equipos: "+output+"\n Empleados: "+output2+"\n Procesos: "+output3)
 
+def detailEquipo (request, equipo_id):
+    equipo=Equipo.objects.get(pk=equipo_id)
+    return HttpResponse(equipo)
 
+def detailEmpleado (request, empleado_id):
+    empleado=Empleado.objects.get(pk=empleado_id)
+    return HttpResponse(empleado)
 
-# Create your views here.
+def detailProceso (request, proceso_id):
+    proceso=Proceso.objects.get(pk=proceso_id)
+    return HttpResponse(proceso)
